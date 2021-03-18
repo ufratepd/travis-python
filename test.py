d8ab8a5a1659a1d36a7c0cedf75d50b472447445
ufratepd <ufratepd@gmail.com>
2021-03-17 21:34:16 -0300

    fix

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ from Calc import somar
 from Calc import subtrair
 
 def test_subtrair():
-    assert subtrair(4,2) == 4
+    assert subtrair(4,2) == 2
 
 def test_somar():
-    assert somar(2,4) == 4
+    assert somar(2,4) == 6
 
 def test_print():
     assert "kli" == "kli"
