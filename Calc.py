be1ff2657e0359bbb2221d86381a35830fe0aca2
ufratepd <ufratepd@gmail.com>
2021-03-18 12:37:01 -0300

    mais tests

--- Calc.py
+++ Calc.py
@@ -1,12 +1,15 @@
 def somar(x,y):
-    """ docstring qq"""
     return (x+y)
-
-
-
 
 def subtrair(x,y):
     return (x-y)
 
+def multiplicar(x,y):
+    return (x*y)
 
-#print(soma(2,3))
+def dividir(x,y):
+    return (x/y)
+
+
+
+
--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 from Calc import somar
 from Calc import subtrair
+from Calc import multiplicar
+from Calc import dividir
 
-def test_subtrair():
-    assert subtrair(4,2) == 2
+
 
 def test_somar():
     assert somar(2,4) == 6
 
-def test_print():
-    assert "kli" == "kli"
+def test_subtrair():
+    assert subtrair(4,2) == 2
+
+def test_multiplicar():
+    assert multiplicar(4,2) == 8
+
+def test_dividir():
+    assert dividir(4,2) == 2
+
 
